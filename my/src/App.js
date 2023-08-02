6e76b78531b2ea4ebac773ec26c6ed327b5f851d
Amish <hajiaamish533@gmail.com>
2023-08-02 11:05:58 -0700

    welcome

---
+++ my/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}>Subscribe Hajia Amish</span>
+    </div>
+  );
+}
+
+export default App;
+
